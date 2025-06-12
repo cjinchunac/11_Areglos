8e9d6cb19970684cad170efa6daad8ae288a4bf3
cjinchunac <cjinchunac@unjbg.edu.pe>
2025-06-11 23:32:28 -0500

    Actualiza ejercicio2: Agrega comentarios y muestra promedio

--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 using namespace std;
 int main () {
-    int n, s, prom, A[20];
+    float n, s, prom;
+    int A[20];
     s=0;
+    cout<<"Ingrese la cantidad de numeros a ingresar"<<endl;
     cin>>n;
     for(int i=0;i<n;i++) {
         cin>>A[i];
         s=s+A[i];
     }
     prom=s/n;
+    cout<<"El promedio de los numeros ingresados es: "<<prom;
+    cout<<endl<<"Los numeros mayores al promedio son"<<endl;
     for(int i=0;i<n;i++) {
         if (A[i]>prom)
             cout<<A[i]<<", ";
